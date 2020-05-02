01847e784884e35d89547cfea0a76e9815d5a7fa
tauan-longaretti <tauanlongaretti@gmail.com>
2020-05-01 21:27:11 -0400

    scafolding to introduce usersModel etc

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,9 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const router = require('express').Router();
+const secrets = require('../config/secrets.js')
+
+const Users = require('../users/usersModel.js');
 
 router.post('/register', (req, res) => {
   // implement registration
