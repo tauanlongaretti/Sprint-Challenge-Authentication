f95d5d9aa54e6f657439aff3ae76304e03985e5a
tauan-longaretti <tauanlongaretti@gmail.com>
2020-05-01 21:54:18 -0400

    authenticate middleware implemented successfully

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,26 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets.js");
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+
+    if (token) {
+      jwt.verify(token, secrets.jwt_secret, (err, decodedToken) => {
+        if (err) {
+          res.status(401).json({ you: "shall not pass" });
+        } else {
+          req.decodedJWT = decodedToken;
+          next();
+        }
+      });
+    } else {
+      throw new Error("invalid auth data");
+    }
+  } catch (err) {
+    res.status(401).json(err.message);
+  }
 };
